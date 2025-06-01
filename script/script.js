3cb4c9165d931821133299db0099e1f73f30ceb6
Mehmet Talha Bozan <talhabozan@gmail.com>
2025-06-02 02:45:45 +0300

    Add burger icon for mobile

--- script/script.js
+++ script/script.js
@@ -1,29 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('burgerMenu')?.addEventListener('click', function () {
-        document.getElementById('navLinks')?.classList.toggle('active');
+document.addEventListener("DOMContentLoaded", function () {
+  var burger = document.getElementById("burgerMenu");
+  var nav = document.getElementById("navLinks");
+
+  if (burger && nav) {
+    burger.addEventListener("click", function () {
+      nav.classList.toggle("open");
     });
+  }
 
-    const testimonials = document.querySelectorAll('.testimonial');
-    let currentTestimonial = 0;
+  const testimonials = document.querySelectorAll(".testimonial");
+  let currentTestimonial = 0;
 
-    function showTestimonial(index) {
-        testimonials.forEach((testimonial, i) => {
-            testimonial.classList.remove('active');
-            if (i === index) {
-                testimonial.classList.add('active');
-            }
-        });
-    }
-
-    document.querySelector('.left')?.addEventListener('click', () => {
-        currentTestimonial = (currentTestimonial === 0) ? testimonials.length - 1 : currentTestimonial - 1;
-        showTestimonial(currentTestimonial);
+  function showTestimonial(index) {
+    testimonials.forEach((testimonial, i) => {
+      testimonial.classList.remove("active");
+      if (i === index) {
+        testimonial.classList.add("active");
+      }
     });
+  }
 
-    document.querySelector('.right')?.addEventListener('click', () => {
-        currentTestimonial = (currentTestimonial === testimonials.length - 1) ? 0 : currentTestimonial + 1;
-        showTestimonial(currentTestimonial);
-    });
-
+  document.querySelector(".left")?.addEventListener("click", () => {
+    currentTestimonial =
+      currentTestimonial === 0
+        ? testimonials.length - 1
+        : currentTestimonial - 1;
     showTestimonial(currentTestimonial);
+  });
+
+  document.querySelector(".right")?.addEventListener("click", () => {
+    currentTestimonial =
+      currentTestimonial === testimonials.length - 1
+        ? 0
+        : currentTestimonial + 1;
+    showTestimonial(currentTestimonial);
+  });
+
+  showTestimonial(currentTestimonial);
 });
